fb0a4cc5b547f4feff57dbefec4ef79240523069
danh7598 <34207162+danh7598@users.noreply.github.com>
2021-04-13 12:10:04 +0700

    Add png file

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { sizeWidth } from './src/utils/Size';
+
 
 export default function App() {
   return (
@@ -17,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: sizeWidth()
   },
 });
