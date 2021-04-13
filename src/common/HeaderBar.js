41553996a2e1526b7f127de67d95729962d51917
danh7598 <34207162+danh7598@users.noreply.github.com>
2021-04-13 12:17:46 +0700

    Fix HeaderBar

---
+++ src/common/HeaderBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HeaderBar = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default HeaderBar
+
+const styles = StyleSheet.create({})
