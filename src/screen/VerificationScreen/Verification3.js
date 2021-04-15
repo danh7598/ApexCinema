e5401df056380b1d4d532b121dac70c1af0dd07e
danh7598 <34207162+danh7598@users.noreply.github.com>
2021-04-15 20:38:57 +0700

    Finish series Verification screen

--- src/screen/VerificationScreen/Verification3.js
+++ src/screen/VerificationScreen/Verification3.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 import back from '../../../assets/Back.png';
 import Button from '../../common/Button';
 import HeaderBar from '../../common/HeaderBar';
@@ -7,11 +7,15 @@ import { neutral3Color, primaryColor } from '../../strings/colors';
 import { sizeFont, sizeHeight, sizeWidth } from '../../utils/Size';
 import smile_face from '../../../assets/smile-face-verification.png';
 import TextField from '../SignInAndUpScreen/TextField';
+import PinCodeInput from './PinCodeInput';
 export default class Verification3 extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            phoneValue: ''
+            pin1: '',
+            pin2: '',
+            pin3: '',
+            pin4: ''
         };
     }
 
@@ -29,12 +33,19 @@ export default class Verification3 extends Component {
                     Please type the vertified code that has been sent to
                     {<Text style={styles.textPhone}> (+1) 280 437 4729</Text>}
                 </Text>
-                <TextField
-                    value={this.state.phoneValue}
-                    onChangeText={(text) => this.onChangePhone(text)}
-                    placeholder={"Enter phone number"}
-                    keyboardType={'phone-pad'}
-                    styleView={styles.viewInput} />
+                <View style={styles.viewPinCode}>
+                    <PinCodeInput />
+                    <PinCodeInput />
+                    <PinCodeInput />
+                    <PinCodeInput />
+                </View>
+                <View style={styles.viewReSend}>
+                    <Text style={styles.textDidnotReceive}>I didn't recieve a code?</Text>
+                    <TouchableOpacity>
+                        <Text style={styles.textResend}>Resend</Text>   
+                    </TouchableOpacity>
+                    
+                </View>
                 <Button
                     styleViewButton={styles.button}
                     textButton={"Next"}
@@ -64,8 +75,26 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginTop: sizeHeight(2.6)
     },
-    viewInput: {
-        marginTop: sizeHeight(7.03)
+    viewPinCode: {
+        marginTop: sizeHeight(7.03),
+        flexDirection: 'row',
+    },
+    viewReSend: {
+        marginTop: sizeHeight(3.6),
+        width: sizeWidth(100),
+        paddingHorizontal: sizeWidth(6.4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    textDidnotReceive: {
+        fontWeight: '400',
+        fontSize: sizeFont(3.73),
+        color: neutral3Color
+    },
+    textResend: {
+        fontWeight: '400',
+        fontSize: sizeFont(3.73),
+        color: primaryColor
     },
     button: {
         position: 'absolute',
